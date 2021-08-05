0dc79dcb0e0e3d91a3307f4724e6b8fcd953221e
nhatlx159 <75898680+nhatlx159@users.noreply.github.com>
2021-08-05 15:23:19 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$ketnoi['host'] = 'localhost'; 
-$ketnoi['dbname'] = 'data'; // Tên database 
-$ketnoi['username'] = 'root'; // Tên user mặc định là root 
-$ketnoi['password'] = ''; // Password để trống 
-@mysql_connect("{$ketnoi['host']}", "{$ketnoi['username']}", "{$ketnoi['password']}") or die("Không thể kết nối database"); 
-@mysql_select_db( "{$ketnoi['dbname']}") or die("Không thể chọn database"); 
-?>
